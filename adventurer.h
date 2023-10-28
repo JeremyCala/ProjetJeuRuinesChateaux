13a911ed07ec328d7b8560820c9942e5d14d4977
Bimrane <149228230+Bimrane@users.noreply.github.com>
2023-10-28 11:43:12 +0200

    Update adventurer.h

--- adventurer.h
+++ adventurer.h
@@ -4,9 +4,11 @@
 class adventurer
 {
 public:
-  adventurer
+  adventurer(int lp, int str, int sword, int armor);
+  int lifepoint
 private:
-    int d_Pv,
+    int d_lp, d_str, ps_sword, 
+    int ps-armor, d_purse;
 };
 
 #endif // ADVENTURER_H
