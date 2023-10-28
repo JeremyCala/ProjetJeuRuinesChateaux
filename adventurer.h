577ba4f871d095cf43f59b46205a00940763c161
Bimrane <149228230+Bimrane@users.noreply.github.com>
2023-10-28 11:50:41 +0200

    ajour classe adventurer.h

--- adventurer.h
+++ adventurer.h
@@ -5,10 +5,16 @@ class adventurer
 {
 public:
   adventurer(int lp, int str, int sword, int armor);
-  int lifepoint
+  int lifepoint() const;
+  int strength() const;
+  int purse() const;
+  int ps_sword() const;
+  int ps_armor() const;
+  position getposition() const;
+  bool amulette() const;
 private:
-    int d_lp, d_str, ps_sword, 
-    int ps-armor, d_purse;
+  int d_lp, d_str, ps_sword, ps-armor, d_purse;
+  position d_pos;  
 };
 
 #endif // ADVENTURER_H
